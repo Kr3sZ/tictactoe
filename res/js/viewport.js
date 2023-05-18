f5b19bf7bbd7ae632bf6e3e2b6260e8a80e8a81b
Krisz <72634331+Kr3sZ@users.noreply.github.com>
2023-05-19 01:29:27 +0200

    simplifying

--- res/js/viewport.js
+++ res/js/viewport.js
@@ -1,23 +1,21 @@
-let game = document.getElementById("game");
-checkViewportSize();
+const GAME = document.getElementById("game");
+const MENU = document.getElementById("menu");
 
-window.addEventListener("resize", checkViewportSize);
+CheckViewportSize();
 
-function checkViewportSize()
+window.addEventListener("resize", CheckViewportSize);
+
+function CheckViewportSize()
 {
     let vw = window.innerWidth;
     let vh = window.innerHeight;
+    
 
-    if (vw > vh)
-    {
-        game.style.height = "100vh";
-        game.style.width = "100vh";
-        console.log(`W:${vw} | H:${vh} | vw > vh`)
-    }  
-    else
-    {
-        game.style.height = "100vw";
-        game.style.width = "100vw";
-        console.log(`W:${vw} | H:${vh} | vw < vh`)
-    }
+    GAME.style.width = "100vmin";
+    GAME.style.height = "100vmin";
+    
+    MENU.style.width = vw > vh ? `${vw-GAME.offsetWidth}px` : "100vw";
+    MENU.style.height = vw > vh ? "100vh" : `${vh-GAME.offsetHeight}px`;
+    
+    console.log(`VIEWPORT W:${vw} | H:${vh}\nGAME     W:${GAME.offsetWidth} | H:${GAME.offsetHeight}\nMENU     W:${MENU.offsetWidth} | H:${MENU.offsetHeight}`);
 }
